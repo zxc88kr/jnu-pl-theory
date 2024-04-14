bb197e2b395b8b56d47604625753f2f2a40b35ea
Park Chan <zxc88kr@naver.com>
2024-04-14 21:46:02 +0900

    Remove White Space

--- Clite-student/AbstractSyntax.java
+++ Clite-student/AbstractSyntax.java
@@ -1,39 +1,35 @@
-// Abstract syntax for the language C++Lite,
-// exactly as it appears in Appendix B.
-
 import java.util.*;
 
 class Program {
-    // Program = Declarations decpart ; Block body
+    // Program = Declarations decpart; Block body
     Declarations decpart;
     Block body;
 
-    Program (Declarations d, Block b) {
-        decpart = d;
-        body = b;
+    Program(Declarations d, Block b) {
+        decpart = d; body = b;
     }
-
+    // 추가 예정
 }
 
 class Declarations extends ArrayList<Declaration> {
     // Declarations = Declaration*
     // (a list of declarations d1, d2, ..., dn)
-
+    // 추가 예정
 }
 
 class Declaration {
-// Declaration = Variable v; Type t
+    // Declaration = Variable v; Type t
     Variable v;
     Type t;
 
-    Declaration (Variable var, Type type) {
+    Declaration(Variable var, Type type) {
         v = var; t = type;
-    } // declaration */
-
+    }
+    // 추가 예정
 }
 
 class Type {
-    // Type = int | bool | char | float 
+    // Type = int | bool | char | float
     final static Type INT = new Type("int");
     final static Type BOOL = new Type("bool");
     final static Type CHAR = new Type("char");
@@ -42,14 +38,14 @@ class Type {
     
     private String id;
 
-    private Type (String t) { id = t; }
+    private Type(String t) { id = t; }
 
-    public String toString ( ) { return id; }
+    public String toString() { return id; }
 }
 
 abstract class Statement {
     // Statement = Skip | Block | Assignment | Conditional | Loop
-
+    // 추가 예정
 }
 
 class Skip extends Statement {
@@ -57,9 +53,9 @@ class Skip extends Statement {
 
 class Block extends Statement {
     // Block = Statement*
-    //         (a Vector of members)
+    // (a Vector of members)
     public ArrayList<Statement> members = new ArrayList<Statement>();
-
+    // 추가 예정
 }
 
 class Assignment extends Statement {
@@ -67,191 +63,190 @@ class Assignment extends Statement {
     Variable target;
     Expression source;
 
-    Assignment (Variable t, Expression e) {
-        target = t;
-        source = e;
+    Assignment(Variable t, Expression e) {
+        target = t; source = e;
     }
-
+    // 추가 예정
 }
 
 class Conditional extends Statement {
-// Conditional = Expression test; Statement thenbranch, elsebranch
+    // Conditional = Expression test; Statement thenbranch, elsebranch
     Expression test;
     Statement thenbranch, elsebranch;
     // elsebranch == null means "if... then"
     
-    Conditional (Expression t, Statement tp) {
-        test = t; thenbranch = tp; elsebranch = new Skip( );
+    Conditional(Expression t, Statement tp) {
+        test = t; thenbranch = tp; elsebranch = new Skip();
     }
     
-    Conditional (Expression t, Statement tp, Statement ep) {
+    Conditional(Expression t, Statement tp, Statement ep) {
         test = t; thenbranch = tp; elsebranch = ep;
     }
-    
+    // 추가 예정
 }
 
 class Loop extends Statement {
-// Loop = Expression test; Statement body
+    // Loop = Expression test; Statement body
     Expression test;
     Statement body;
 
-    Loop (Expression t, Statement b) {
+    Loop(Expression t, Statement b) {
         test = t; body = b;
     }
-    
+    // 추가 예정
 }
 
 abstract class Expression {
     // Expression = Variable | Value | Binary | Unary
-
+    // 추가 예정
 }
 
 class Variable extends Expression {
     // Variable = String id
     private String id;
 
-    Variable (String s) { id = s; }
+    Variable(String s) { id = s; }
 
-    public String toString( ) { return id; }
+    public String toString() { return id; }
     
-    public boolean equals (Object obj) {
+    public boolean equals(Object obj) {
         String s = ((Variable) obj).id;
         return id.equals(s); // case-sensitive identifiers
     }
     
-    public int hashCode ( ) { return id.hashCode( ); }
-
+    public int hashCode() { return id.hashCode(); }
+    // 추가 예정
 }
 
 abstract class Value extends Expression {
-    // Value = IntValue | BoolValue |
-    //         CharValue | FloatValue
+    // Value = IntValue | BoolValue | CharValue | FloatValue
     protected Type type;
     protected boolean undef = true;
 
-    int intValue ( ) {
+    int intValue() {
         assert false : "should never reach here";
         return 0;
     }
     
-    boolean boolValue ( ) {
+    boolean boolValue() {
         assert false : "should never reach here";
         return false;
     }
     
-    char charValue ( ) {
+    char charValue() {
         assert false : "should never reach here";
         return ' ';
     }
     
-    float floatValue ( ) {
+    float floatValue() {
         assert false : "should never reach here";
         return 0.0f;
     }
 
-    boolean isUndef( ) { return undef; }
+    boolean isUndef() { return undef; }
 
-    Type type ( ) { return type; }
+    Type type() { return type; }
 
-    static Value mkValue (Type type) {
-        if (type == Type.INT) return new IntValue( );
-        if (type == Type.BOOL) return new BoolValue( );
-        if (type == Type.CHAR) return new CharValue( );
-        if (type == Type.FLOAT) return new FloatValue( );
+    static Value mkValue(Type type) {
+        if (type == Type.INT) return new IntValue();
+        if (type == Type.BOOL) return new BoolValue();
+        if (type == Type.CHAR) return new CharValue();
+        if (type == Type.FLOAT) return new FloatValue();
         throw new IllegalArgumentException("Illegal type in mkValue");
     }
+    // 추가 예정
 }
 
 class IntValue extends Value {
     private int value = 0;
 
-    IntValue ( ) { type = Type.INT; }
+    IntValue() { type = Type.INT; }
 
-    IntValue (int v) { this( ); value = v; undef = false; }
+    IntValue(int v) { this(); value = v; undef = false; }
 
-    int intValue ( ) {
+    int intValue() {
         assert !undef : "reference to undefined int value";
         return value;
     }
 
-    public String toString( ) {
-        if (undef)  return "undef";
+    public String toString() {
+        if (undef) return "undef";
         return "" + value;
     }
-
+    // 추가 예정
 }
 
 class BoolValue extends Value {
     private boolean value = false;
 
-    BoolValue ( ) { type = Type.BOOL; }
+    BoolValue() { type = Type.BOOL; }
 
-    BoolValue (boolean v) { this( ); value = v; undef = false; }
+    BoolValue(boolean v) { this(); value = v; undef = false; }
 
-    boolean boolValue ( ) {
+    boolean boolValue() {
         assert !undef : "reference to undefined bool value";
         return value;
     }
 
-    int intValue ( ) {
+    int intValue() {
         assert !undef : "reference to undefined bool value";
         return value ? 1 : 0;
     }
 
-    public String toString( ) {
-        if (undef)  return "undef";
+    public String toString() {
+        if (undef) return "undef";
         return "" + value;
     }
-
+    // 추가 예정
 }
 
 class CharValue extends Value {
     private char value = ' ';
 
-    CharValue ( ) { type = Type.CHAR; }
+    CharValue() { type = Type.CHAR; }
 
-    CharValue (char v) { this( ); value = v; undef = false; }
+    CharValue(char v) { this(); value = v; undef = false; }
 
-    char charValue ( ) {
+    char charValue() {
         assert !undef : "reference to undefined char value";
         return value;
     }
 
-    public String toString( ) {
-        if (undef)  return "undef";
+    public String toString() {
+        if (undef) return "undef";
         return "" + value;
     }
-
+    // 추가 예정
 }
 
 class FloatValue extends Value {
     private float value = 0;
 
-    FloatValue ( ) { type = Type.FLOAT; }
+    FloatValue() { type = Type.FLOAT; }
 
-    FloatValue (float v) { this( ); value = v; undef = false; }
+    FloatValue(float v) { this(); value = v; undef = false; }
 
-    float floatValue ( ) {
+    float floatValue() {
         assert !undef : "reference to undefined float value";
         return value;
     }
 
-    public String toString( ) {
-        if (undef)  return "undef";
+    public String toString() {
+        if (undef) return "undef";
         return "" + value;
     }
-
+    // 추가 예정
 }
 
 class Binary extends Expression {
-// Binary = Operator op; Expression term1, term2
+    // Binary = Operator op; Expression term1, term2
     Operator op;
     Expression term1, term2;
 
-    Binary (Operator o, Expression l, Expression r) {
+    Binary(Operator o, Expression l, Expression r) {
         op = o; term1 = l; term2 = r;
-    } // binary
-
+    }
+    // 추가 예정
 }
 
 class Unary extends Expression {
@@ -259,10 +254,10 @@ class Unary extends Expression {
     Operator op;
     Expression term;
 
-    Unary (Operator o, Expression e) {
+    Unary(Operator o, Expression e) {
         op = o; term = e;
-    } // unary
-
+    }
+    // 추가 예정
 }
 
 class Operator {
@@ -282,7 +277,7 @@ class Operator {
     final static String MINUS = "-";
     final static String TIMES = "*";
     final static String DIV = "/";
-    // UnaryOp = !    
+    // UnaryOp = ! | -
     final static String NOT = "!";
     final static String NEG = "-";
     // CastOp = int | float | char
@@ -302,7 +297,7 @@ class Operator {
     final static String INT_MINUS = "INT-";
     final static String INT_TIMES = "INT*";
     final static String INT_DIV = "INT/";
-    // UnaryOp = !    
+    // UnaryOp = -
     final static String INT_NEG = "-";
     // RelationalOp = < | <= | == | != | >= | >
     final static String FLOAT_LT = "FLOAT<";
@@ -316,7 +311,7 @@ class Operator {
     final static String FLOAT_MINUS = "FLOAT-";
     final static String FLOAT_TIMES = "FLOAT*";
     final static String FLOAT_DIV = "FLOAT/";
-    // UnaryOp = !    
+    // UnaryOp = -
     final static String FLOAT_NEG = "-";
     // RelationalOp = < | <= | == | != | >= | >
     final static String CHAR_LT = "CHAR<";
@@ -340,27 +335,27 @@ class Operator {
     
     String val;
     
-    Operator (String s) { val = s; }
+    Operator(String s) { val = s; }
 
-    public String toString( ) { return val; }
+    public String toString() { return val; }
     public boolean equals(Object obj) { return val.equals(obj); }
     
-    boolean BooleanOp ( ) { return val.equals(AND) || val.equals(OR); }
-    boolean RelationalOp ( ) {
+    boolean BooleanOp() { return val.equals(AND) || val.equals(OR); }
+    boolean RelationalOp() {
         return val.equals(LT) || val.equals(LE) || val.equals(EQ)
             || val.equals(NE) || val.equals(GT) || val.equals(GE);
     }
-    boolean ArithmeticOp ( ) {
+    boolean ArithmeticOp() {
         return val.equals(PLUS) || val.equals(MINUS)
             || val.equals(TIMES) || val.equals(DIV);
     }
-    boolean NotOp ( ) { return val.equals(NOT) ; }
-    boolean NegateOp ( ) { return val.equals(NEG) ; }
-    boolean intOp ( ) { return val.equals(INT); }
-    boolean floatOp ( ) { return val.equals(FLOAT); }
-    boolean charOp ( ) { return val.equals(CHAR); }
+    boolean NotOp() { return val.equals(NOT); }
+    boolean NegateOp() { return val.equals(NEG); }
+    boolean intOp() { return val.equals(INT); }
+    boolean floatOp() { return val.equals(FLOAT); }
+    boolean charOp() { return val.equals(CHAR); }
 
-    final static String intMap[ ] [ ] = {
+    final static String intMap[][] = {
         {PLUS, INT_PLUS}, {MINUS, INT_MINUS},
         {TIMES, INT_TIMES}, {DIV, INT_DIV},
         {EQ, INT_EQ}, {NE, INT_NE}, {LT, INT_LT},
@@ -368,7 +363,7 @@ class Operator {
         {NEG, INT_NEG}, {FLOAT, I2F}, {CHAR, I2C}
     };
 
-    final static String floatMap[ ] [ ] = {
+    final static String floatMap[][] = {
         {PLUS, FLOAT_PLUS}, {MINUS, FLOAT_MINUS},
         {TIMES, FLOAT_TIMES}, {DIV, FLOAT_DIV},
         {EQ, FLOAT_EQ}, {NE, FLOAT_NE}, {LT, FLOAT_LT},
@@ -376,18 +371,18 @@ class Operator {
         {NEG, FLOAT_NEG}, {INT, F2I}
     };
 
-    final static String charMap[ ] [ ] = {
+    final static String charMap[][] = {
         {EQ, CHAR_EQ}, {NE, CHAR_NE}, {LT, CHAR_LT},
         {LE, CHAR_LE}, {GT, CHAR_GT}, {GE, CHAR_GE},
         {INT, C2I}
     };
 
-    final static String boolMap[ ] [ ] = {
+    final static String boolMap[][] = {
         {EQ, BOOL_EQ}, {NE, BOOL_NE}, {LT, BOOL_LT},
         {LE, BOOL_LE}, {GT, BOOL_GT}, {GE, BOOL_GE},
     };
 
-    final static private Operator map (String[][] tmap, String op) {
+    final static private Operator map(String[][] tmap, String op) {
         for (int i = 0; i < tmap.length; i++)
             if (tmap[i][0].equals(op))
                 return new Operator(tmap[i][1]);
@@ -395,20 +390,20 @@ class Operator {
         return null;
     }
 
-    final static public Operator intMap (String op) {
-        return map (intMap, op);
+    final static public Operator intMap(String op) {
+        return map(intMap, op);
     }
 
-    final static public Operator floatMap (String op) {
-        return map (floatMap, op);
+    final static public Operator floatMap(String op) {
+        return map(floatMap, op);
     }
 
-    final static public Operator charMap (String op) {
-        return map (charMap, op);
+    final static public Operator charMap(String op) {
+        return map(charMap, op);
     }
 
-    final static public Operator boolMap (String op) {
-        return map (boolMap, op);
+    final static public Operator boolMap(String op) {
+        return map(boolMap, op);
     }
-
+    // 추가 예정
 }
--- Clite-student/Parser.java
+++ Clite-student/Parser.java
@@ -1,20 +1,20 @@
 import java.util.*;
 
 public class Parser {
-    // Recursive descent parser that inputs a C++Lite program and 
+    // Recursive descent parser that inputs a C++Lite program and
     // generates its abstract syntax.  Each method corresponds to
     // a concrete syntax grammar rule, which appears as a comment
     // at the beginning of the method.
   
-    Token token;          // current token from the input stream
+    Token token; // current token from the input stream
     Lexer lexer;
-  
+
     public Parser(Lexer ts) { // Open the C++Lite source program
-        lexer = ts;                          // as a token stream, and
-        token = lexer.next();            // retrieve its first Token
+        lexer = ts; // as a token stream, and
+        token = lexer.next(); // retrieve its first Token
     }
   
-    private String match (TokenType t) {
+    private String match(TokenType t) {
         String value = token.value();
         if (token.type().equals(t))
             token = lexer.next();
@@ -24,96 +24,96 @@ public class Parser {
     }
   
     private void error(TokenType tok) {
-        System.err.println("Syntax error: expecting: " + tok 
-                           + "; saw: " + token);
+        System.err.println("Syntax error: expecting: " + tok + "; saw: " + token);
         System.exit(1);
     }
   
     private void error(String tok) {
-        System.err.println("Syntax error: expecting: " + tok 
-                           + "; saw: " + token);
+        System.err.println("Syntax error: expecting: " + tok + "; saw: " + token);
         System.exit(1);
     }
   
     public Program program() {
-        // Program --> void main ( ) '{' Declarations Statements '}'
-        TokenType[ ] header = {TokenType.Int, TokenType.Main,
-                          TokenType.LeftParen, TokenType.RightParen};
-        for (int i=0; i<header.length; i++)   // bypass "int main ( )"
+        // Program --> void main() '{' Declarations Statements '}'
+        TokenType[] header = {TokenType.Int, TokenType.Main,
+                              TokenType.LeftParen, TokenType.RightParen};
+        for (int i = 0; i < header.length; i++) { // bypass "int main()"
             match(header[i]);
+        }
         match(TokenType.LeftBrace);
-        // student exercise
+        // 추가 예정
         match(TokenType.RightBrace);
-        return null;  // student exercise
+        return null; // 추가 예정
     }
   
-    private Declarations declarations () {
+    private Declarations declarations() {
         // Declarations --> { Declaration }
-        return null;  // student exercise
+        // 추가 예정
+        return null; // 추가 예정
     }
   
-    private void declaration (Declarations ds) {
-        // Declaration  --> Type Identifier { , Identifier } ;
-        // student exercise
+    private void declaration(Declarations ds) {
+        // Declaration --> Type Identifier {, Identifier };
+        // 추가 예정
     }
   
-    private Type type () {
-        // Type  -->  int | bool | float | char 
+    private Type type() {
+        // Type --> int | bool | float | char
         Type t = null;
-        // student exercise
+        // 추가 예정
         return t;          
     }
   
     private Statement statement() {
         // Statement --> ; | Block | Assignment | IfStatement | WhileStatement
         Statement s = new Skip();
-        // student exercise
+        // 추가 예정
         return s;
     }
   
-    private Block statements () {
+    private Block statements() {
         // Block --> '{' Statements '}'
         Block b = new Block();
-        // student exercise
+        // 추가 예정
         return b;
     }
   
-    private Assignment assignment () {
-        // Assignment --> Identifier = Expression ;
-        return null;  // student exercise
+    private Assignment assignment() {
+        // Assignment --> Identifier = Expression;
+        return null; // 추가 예정
     }
   
-    private Conditional ifStatement () {
+    private Conditional ifStatement() {
         // IfStatement --> if ( Expression ) Statement [ else Statement ]
-        return null;  // student exercise
+        return null; // 추가 예정
     }
   
-    private Loop whileStatement () {
+    private Loop whileStatement() {
         // WhileStatement --> while ( Expression ) Statement
-        return null;  // student exercise
+        return null; // 추가 예정
     }
 
-    private Expression expression () {
+    private Expression expression() {
         // Expression --> Conjunction { || Conjunction }
-        return null;  // student exercise
+        return null; // 추가 예정
     }
   
-    private Expression conjunction () {
+    private Expression conjunction() {
         // Conjunction --> Equality { && Equality }
-        return null;  // student exercise
+        return null; // 추가 예정
     }
   
-    private Expression equality () {
+    private Expression equality() {
         // Equality --> Relation [ EquOp Relation ]
-        return null;  // student exercise
+        return null; // 추가 예정
     }
 
-    private Expression relation (){
+    private Expression relation() {
         // Relation --> Addition [RelOp Addition] 
-        return null;  // student exercise
+        return null; // 추가 예정
     }
   
-    private Expression addition () {
+    private Expression addition() {
         // Addition --> Term { AddOp Term }
         Expression e = term();
         while (isAddOp()) {
@@ -124,7 +124,7 @@ public class Parser {
         return e;
     }
   
-    private Expression term () {
+    private Expression term() {
         // Term --> Factor { MultiplyOp Factor }
         Expression e = factor();
         while (isMultiplyOp()) {
@@ -145,9 +145,8 @@ public class Parser {
         else return primary();
     }
   
-    private Expression primary () {
-        // Primary --> Identifier | Literal | ( Expression )
-        //             | Type ( Expression )
+    private Expression primary() {
+        // Primary --> Identifier | Literal | ( Expression ) | Type ( Expression )
         Expression e = null;
         if (token.type().equals(TokenType.Identifier)) {
             e = new Variable(match(TokenType.Identifier));
@@ -155,9 +154,9 @@ public class Parser {
             e = literal();
         } else if (token.type().equals(TokenType.LeftParen)) {
             token = lexer.next();
-            e = expression();       
+            e = expression();
             match(TokenType.RightParen);
-        } else if (isType( )) {
+        } else if (isType()) {
             Operator op = new Operator(match(token.type()));
             match(TokenType.LeftParen);
             Expression term = expression();
@@ -167,61 +166,59 @@ public class Parser {
         return e;
     }
 
-    private Value literal( ) {
-        return null;  // student exercise
+    private Value literal() {
+        return null; // 추가 예정
     }
-  
 
-    private boolean isAddOp( ) {
+    private boolean isAddOp() {
         return token.type().equals(TokenType.Plus) ||
                token.type().equals(TokenType.Minus);
     }
     
-    private boolean isMultiplyOp( ) {
+    private boolean isMultiplyOp() {
         return token.type().equals(TokenType.Multiply) ||
                token.type().equals(TokenType.Divide);
     }
     
-    private boolean isUnaryOp( ) {
+    private boolean isUnaryOp() {
         return token.type().equals(TokenType.Not) ||
                token.type().equals(TokenType.Minus);
     }
     
-    private boolean isEqualityOp( ) {
+    private boolean isEqualityOp() {
         return token.type().equals(TokenType.Equals) ||
-            token.type().equals(TokenType.NotEqual);
+               token.type().equals(TokenType.NotEqual);
     }
     
-    private boolean isRelationalOp( ) {
+    private boolean isRelationalOp() {
         return token.type().equals(TokenType.Less) ||
-               token.type().equals(TokenType.LessEqual) || 
+               token.type().equals(TokenType.LessEqual) ||
                token.type().equals(TokenType.Greater) ||
                token.type().equals(TokenType.GreaterEqual);
     }
     
-    private boolean isType( ) {
-        return token.type().equals(TokenType.Int)
-            || token.type().equals(TokenType.Bool) 
-            || token.type().equals(TokenType.Float)
-            || token.type().equals(TokenType.Char);
+    private boolean isType() {
+        return token.type().equals(TokenType.Int) ||
+               token.type().equals(TokenType.Bool) ||
+               token.type().equals(TokenType.Float) ||
+               token.type().equals(TokenType.Char);
     }
     
-    private boolean isLiteral( ) {
+    private boolean isLiteral() {
         return token.type().equals(TokenType.IntLiteral) ||
-            isBooleanLiteral() ||
-            token.type().equals(TokenType.FloatLiteral) ||
-            token.type().equals(TokenType.CharLiteral);
+               isBooleanLiteral() ||
+               token.type().equals(TokenType.FloatLiteral) ||
+               token.type().equals(TokenType.CharLiteral);
     }
     
-    private boolean isBooleanLiteral( ) {
+    private boolean isBooleanLiteral() {
         return token.type().equals(TokenType.True) ||
-            token.type().equals(TokenType.False);
+               token.type().equals(TokenType.False);
     }
     
     public static void main(String args[]) {
-        Parser parser  = new Parser(new Lexer(args[0]));
+        Parser parser = new Parser(new Lexer(args[0]));
         Program prog = parser.program();
-        prog.display();           // display abstract syntax tree
-    } //main
-
-} // Parser
+        prog.display();
+    }
+}
