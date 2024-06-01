352f2088878b2dea81680564f712c717e715481c
Park Chan <zxc88kr@naver.com>
2024-06-01 17:09:03 +0900

    feat: Complete TypeMap

--- Clite-student/TypeMap.java
+++ Clite-student/TypeMap.java
@@ -1,7 +1,29 @@
 import java.util.*;
 
 public class TypeMap extends HashMap<Variable, Type> {
+    public TypeMap onion(TypeMap tm) {
+        TypeMap res = new TypeMap();
+        res.putAll(this);
+        res.putAll(tm);
+        return res;
+    }
+    
     public void display() {
-        System.out.println(this.entrySet());
+        System.out.println();
+        System.out.print("{ ");
+        String sep = "";
+        for (Variable key : keySet()) {
+            System.out.print(sep + "<" + key + ", " );
+            Type t = this.get(key);
+            if (t instanceof Prototype) {
+                System.out.print(((Prototype)t).toString() + ", ");
+                ((Prototype)t).params.display(1);
+                System.out.print(">");
+            } else {
+                System.out.print(get(key).toString() + ">"); 
+            }
+            sep = ", \n";
+        }
+        System.out.println(" }");
     }
 }
